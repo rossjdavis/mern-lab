0a7735287222a6256adeca790fc6cd30c5f90f1f
Sloan Holzman <sloan.holzman@gmail.com>
2017-12-13 15:49:00 -0500

    add downvote

--- index.js
+++ index.js
@@ -67,6 +67,35 @@ app.put("/api/favorite-things/increase-rank/:id", (req, res) => {
     })
 })
 
+app.put("/api/favorite-things/decrease-rank/:id", (req, res) => {
+  let currentRank = 0
+  FavoriteThing.findById(req.params.id)
+    .then(favoriteThing => {
+      console.log(`old item: ${favoriteThing.description}`)
+      currentRank = favoriteThing.rank
+      lowerRank = (currentRank + 1)
+      console.log(`higher rank: ${higherRank}`)
+      FavoriteThing.findOneAndUpdate(
+        {rank: lowerRank },
+        {rank: currentRank},
+        {new: true}
+      )
+    .then((updatedRecord) => {
+      console.log(updatedRecord)
+      FavoriteThing.findOneAndUpdate(
+        { _id: req.params.id },
+        { rank: lowerRank },
+        { new: true }
+      )
+    .then((updatedRecord) => {
+      console.log(updatedRecord)}
+    )})})
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+
 // app.get("api/favorite-things/:id", (req, res) => {
 //   FavoriteThing.findById(req.params.id)
 //     .then(favoriteThing => {
